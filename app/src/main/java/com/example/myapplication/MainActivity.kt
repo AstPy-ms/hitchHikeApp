c4babd3185cfdd5ed9e655a6ead3f9b637beab68
AstPy-ms <sketdance551@gmail.com>
2020-01-05 16:36:36 +0900

    Add some highways

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // ボタン押したら遷移する
-        sendButton.setOnClickListener {
+        toumei.setOnClickListener {
             val intent = Intent(application, SubActivity::class.java)
             startActivity(intent)
         }
